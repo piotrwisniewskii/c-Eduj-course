2377e52b7bee653d807b0b819867627a704d505f
Piotr Wisniewski <piowisni@gmail.com>
2022-10-25 19:26:42 +0200

    wyrażenie Lambda

--- EDUJ/Program.cs
+++ EDUJ/Program.cs
@@ -10,53 +10,15 @@ namespace EDUJ;
 public class Program
 {
 
-    public delegate int CalcDelegate(int a, int b);
-    public delegate string StringDelegate(string a);
-
     static void Main(string[] args)
     {
-        CalcDelegate calcDelegate = Add;
-        CalcWithText(calcDelegate.Invoke, 2, 5);
-
-        StringDelegate stringDelegate = Hello;
-        PrzywitajSie(stringDelegate.Invoke, "siemano");
-
-        string czesc = "ELO";
-        StringDelegate stringDelegate2 = delegate (string czesc)
+    
+        CalcWithText(delegate (int a, int b)
         {
-            return "hello";
-        };
-        PrzywitajSie(stringDelegate2.Invoke, czesc);
-        Console.WriteLine(stringDelegate2.Invoke(czesc));
-        CalcDelegate calcDelegate2 = delegate (int a, int b) // metoda ANONIMOWA tzn. "jednorazowa"
-        { 
             return a + b;
-        };
-        Console.WriteLine(calcDelegate2.Invoke(2,5));
-        CalcWithText(calcDelegate2.Invoke, 2, 5);
+        }, 2, 5); 
 
-
-        string a = "turur";
-        StringDelegate stringo = delegate (string a)
-        {
-            return a;
-        };
-        Console.WriteLine(stringo.Invoke(a));
-
-        Func<int, int, int> funcDelegate = Add;
-        CalcWithText(funcDelegate.Invoke, 2, 5);
-
-        Action<int, int> action = delegate (int a, int b)
-        {
-            Console.WriteLine(a + b);
-        };
-
-        Predicate<int> predicate = delegate (int a)
-        {
-            return a == 0;
-        };
-        Console.WriteLine(predicate.Invoke(5));
-        Console.WriteLine(predicate.Invoke(0));
+        CalcWithText((x, y) =>  x + y, 2, 5);
     }
 
     static void CalcWithText(Func<int, int, int> func, int a, int b )
@@ -65,23 +27,6 @@ public class Program
         Console.WriteLine($"Wynik = {res}");
     }
 
-    static void PrzywitajSie(Func<string, string>func, string czesc)
-    {
-        string h = func.Invoke(czesc);
-    }
-
-    static string Hello(string czesc)
-    {
-        return czesc;
-    }
-
-
-    static int Add(int a,int b)
-    {
-        return a + b;
-    }
-
-    
     
 }
 
