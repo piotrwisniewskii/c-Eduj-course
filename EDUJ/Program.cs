475773210c59fc29649cffb81ffd41a643b9b0ce
Piotr Wisniewski <piowisni@gmail.com>
2022-10-25 17:13:22 +0200

    Metoda jako parametr

--- EDUJ/Program.cs
+++ EDUJ/Program.cs
@@ -11,13 +11,19 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        CalcWithText(Add, 2, 5);
 
     }
 
     static void CalcWithText(Func<int,int,int> func, int a,int b)
     {
+        int res = func(a, b);
+        Console.WriteLine($"Wynik = {res}");
+    }
 
+    static int Add(int a, int b)
+    {
+        return a + b;
     }
     
 }
