ae9a848a82f5c460bda5877cc43c488447df5ad3
Piotr Wisniewski <piowisni@gmail.com>
2022-10-28 21:21:06 +0200

    HashSet SortedSet

--- EDUJ/Program.cs
+++ EDUJ/Program.cs
@@ -12,19 +12,59 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Thread t1 = new Thread(new ParameterizedThreadStart(LongMethod));
-        Thread t2 = new Thread(new ParameterizedThreadStart(LongMethod));
-        t1.Start(50);
-        t2.Start(50);
+        HashSet<string> hs = new HashSet<string>();
+        hs.Add("Poniedziałek");
+        hs.Add("Wtorek");
+        hs.Add("Wtorek");
+        hs.Add("Środa");
+        hs.Add("Czwartek");
+        hs.Add("Piątek");
+        hs.Add("Piątek");
+        hs.Add("Piątek");
+
+        Console.WriteLine(hs.Contains("Poniedziałek"));
+        Console.WriteLine(hs.Contains("Niedziela"));
+        Console.WriteLine("\nHashSet - Ilość " + hs.Count);
+
+        foreach (var item in hs)
+        {
+            Console.WriteLine(item);
+        }
+
+        SortedSet<string> ss = new SortedSet<string>();
+        ss.Add("Piątek");
+        ss.Add("Sobota");
+        ss.Add("Niedziela");
+        ss.Add("Niedziela");
+        ss.Add("Niedziela");
+        ss.Add("Niedziela");
+
+        Console.WriteLine("\nSortedSet - Ilość " + ss.Count);
+
+        foreach (var item in ss)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("\nOperacje:");
+
+        hs.UnionWith(ss); // połączenie
+        hs.ExceptWith(ss); // usunięcie
+        hs.Intersect(ss); // część wspólna
+        hs.SymmetricExceptWith(ss); // częśc wspołna 2 zbiorów z wykluczeniem powtórzeń
+
+        foreach (var item in hs)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("\nHashSet - Ilość " + hs.Count);
+        Console.WriteLine(hs.IsSubsetOf(hs));
+        Console.WriteLine(hs.IsSupersetOf(ss));
+
+
     }
 
-    static void LongMethod(object n)
-    {
-        for (int i = 0; i < (int)n; i++)
-        {
-            Console.WriteLine($" ID {Thread.CurrentThread.ManagedThreadId}: {i}");
-        }
-    }
+    
 }
 
 
