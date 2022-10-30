70ffd076845bfab901ca08917678e6b04438c943
Piotr Wisniewski <piowisni@gmail.com>
2022-10-30 10:54:21 +0100

    ArrayList

--- EDUJ/Program.cs
+++ EDUJ/Program.cs
@@ -1,8 +1,10 @@
 ﻿
 using System;
+using System.Collections;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Security;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -12,59 +14,43 @@ public class Program
 {
     static void Main(string[] args)
     {
-        HashSet<string> hs = new HashSet<string>();
-        hs.Add("Poniedziałek");
-        hs.Add("Wtorek");
-        hs.Add("Wtorek");
-        hs.Add("Środa");
-        hs.Add("Czwartek");
-        hs.Add("Piątek");
-        hs.Add("Piątek");
-        hs.Add("Piątek");
+        ArrayList arrayList = new ArrayList(5);
+        arrayList.Add(5);
+        arrayList.Add(15);
+        arrayList.Add(-5);
+        arrayList.Add(10);
+        arrayList.Remove(5);
+        arrayList.RemoveAt(1);
 
-        Console.WriteLine(hs.Contains("Poniedziałek"));
-        Console.WriteLine(hs.Contains("Niedziela"));
-        Console.WriteLine("\nHashSet - Ilość " + hs.Count);
-
-        foreach (var item in hs)
+        arrayList.Sort();
+        arrayList.Reverse();
+        arrayList.Clear();
+        Console.WriteLine("Ilość elementów: " + arrayList.Count);
+        //Console.WriteLine(arrayList[0]);
+        //Console.WriteLine(arrayList[1]);
+        Animal Kot = new Animal("Diana", 1);
+        arrayList.Insert(0, Kot);
+        Console.WriteLine("Ilość elementów: " + arrayList.Count);
+        foreach (var item in arrayList)
         {
+            
+            if ( item is Animal)
+            {
+                Console.WriteLine(((Animal)item).name);
+               
+                    
+            }
             Console.WriteLine(item);
         }
-
-        SortedSet<string> ss = new SortedSet<string>();
-        ss.Add("Piątek");
-        ss.Add("Sobota");
-        ss.Add("Niedziela");
-        ss.Add("Niedziela");
-        ss.Add("Niedziela");
-        ss.Add("Niedziela");
-
-        Console.WriteLine("\nSortedSet - Ilość " + ss.Count);
-
-        foreach (var item in ss)
-        {
-            Console.WriteLine(item);
-        }
-
-        Console.WriteLine("\nOperacje:");
-
-        hs.UnionWith(ss); // połączenie
-        hs.ExceptWith(ss); // usunięcie
-        hs.Intersect(ss); // część wspólna
-        hs.SymmetricExceptWith(ss); // częśc wspołna 2 zbiorów z wykluczeniem powtórzeń
-
-        foreach (var item in hs)
-        {
-            Console.WriteLine(item);
-        }
-        Console.WriteLine("\nHashSet - Ilość " + hs.Count);
-        Console.WriteLine(hs.IsSubsetOf(hs));
-        Console.WriteLine(hs.IsSupersetOf(ss));
-
-
     }
+}
 
-    
+class Testtt
+{
+    public void SayHi()
+    {
+        Console.WriteLine("Hello!");
+    }
 }
 
 
