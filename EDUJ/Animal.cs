ad1d121fdd1b76fca640345e2a7936f36a3d400a
Piotr Wisniewski <piowisni@gmail.com>
2022-10-30 12:28:48 +0100

    List

--- EDUJ/Animal.cs
+++ EDUJ/Animal.cs
@@ -4,34 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EDUJ
-{
-    public class Animal : Object // DZIEDZICZENIE NIEJAWNE
-    {
-       public string name;
-        public int age;
-        public void Show()
-        {
-            Console.Write($"Imie: {name}, wiek: {age}");
-        }
-        public Animal(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-        }
-    }
-    public class Cat : Animal
-    {
-        public string color;
-        public void Show()
-        {
-            base.Show();
-            Console.WriteLine($"kolor {color}");
-        }
+//namespace EDUJ
+//{
+//    public class Animal : Object // DZIEDZICZENIE NIEJAWNE
+//    {
+//       public string name;
+//        public int age;
+//        public void Show()
+//        {
+//            Console.Write($"Imie: {name}, wiek: {age}");
+//        }
+//        public Animal(string name, int age)
+//        {
+//            this.name = name;
+//            this.age = age;
+//        }
+//    }
+//    public class Cat : Animal
+//    {
+//        public string color;
+//        public void Show()
+//        {
+//            base.Show();
+//            Console.WriteLine($"kolor {color}");
+//        }
 
-        public Cat(string name, int age, string color) : base(name,age)
-        {
-            this.color = color;
-        }
-    }
-} 
+//        public Cat(string name, int age, string color) : base(name,age)
+//        {
+//            this.color = color;
+//        }
+//    }
+//} 
--- EDUJ/Program.cs
+++ EDUJ/Program.cs
@@ -1,57 +1,95 @@
 ﻿
-using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Net.Security;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-
+using System.ComponentModel;
 
 namespace EDUJ;
 public class Program
 {
     static void Main(string[] args)
     {
-        ArrayList arrayList = new ArrayList(5);
-        arrayList.Add(5);
-        arrayList.Add(15);
-        arrayList.Add(-5);
-        arrayList.Add(10);
-        arrayList.Remove(5);
-        arrayList.RemoveAt(1);
-
-        arrayList.Sort();
-        arrayList.Reverse();
-        arrayList.Clear();
-        Console.WriteLine("Ilość elementów: " + arrayList.Count);
-        //Console.WriteLine(arrayList[0]);
-        //Console.WriteLine(arrayList[1]);
-        Animal Kot = new Animal("Diana", 1);
-        arrayList.Insert(0, Kot);
-        Console.WriteLine("Ilość elementów: " + arrayList.Count);
-        foreach (var item in arrayList)
+        List<Kotki> kotki = new List<Kotki>();
+        Kotki Diana = new Kotki("Diana", 10, 30);
+        Kotki Dio = new Kotki("Dio", 100, 20000);
+        Kotki Leo = new Kotki("Leo", 20, 200);
+        Kotki Bobek = new Kotki("Bobek", 1, 1);
+        Kotki Miałczyńsk = new Kotki("Miałczyńska", 110, 30);
+        Kotki KotaZewnetrzna = new Kotki("KotaZewnetrzna", 100, 100);
+        kotki.Add(Diana);
+        kotki.Add(Dio);
+        kotki.Add(Leo);
+        kotki.Add(Bobek);
+        kotki.Add(Miałczyńsk);
+        kotki.Add(KotaZewnetrzna);
+       
+        foreach (Kotki kotek in kotki)
         {
             
-            if ( item is Animal)
-            {
-                Console.WriteLine(((Animal)item).name);
-               
-                    
-            }
-            Console.WriteLine(item);
+            Console.WriteLine(kotek.Name);
+            
         }
+
+        Random random = new Random();
+        
+        
+        
+        Console.WriteLine($"Pierwsza Walka!{Dio.Name} kontra {Leo.Name}");
+        do
+        {
+            int atak = random.Next(1, 10);
+            if (Leo.Zycie > 0)
+            {
+                Dio.Attack(Leo, atak);
+                
+            }
+            else
+                Console.WriteLine("Leo Ucieka w popłochu!");
+            if(Dio.Zycie > 0)
+            {
+                Leo.Attack(Dio, atak);
+            }
+            else
+                Console.WriteLine("Dio Ucieka w popłochu!");
+
+        } while (Dio.Zycie > 0 && Leo.Zycie > 0);
+        Console.WriteLine($"Walka zakonczona Dio : {Dio.Zycie} Leo : {Leo.Zycie} ");
+       
+
+
+
+
+    }
+
+    public class Kotki
+    {
+        public string Name { get; set; }
+        public int Sila { get; set; }
+        public int Zycie { get; set; }
+
+        public Kotki(string name, int sila, int zycie)
+        {
+            Name = name;
+            Sila = sila;
+            Zycie = zycie;
+        }
+
+        public int Attack(Kotki kotek, int dmg)
+        {
+            kotek.Zycie -= dmg;
+            Console.WriteLine($"Oh nie {kotek.Name} oberwał za {dmg} i ma aktualnie {kotek.Zycie}");
+            return kotek.Zycie;
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Helo");
+        }
+
     }
 }
 
-class Testtt
-{
-    public void SayHi()
-    {
-        Console.WriteLine("Hello!");
-    }
-}
+
+
+
+
 
 
 
